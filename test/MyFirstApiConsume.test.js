94ff1b3c63779182795a37a07c17e1c76afdf10e
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-03-15 20:44:55 -0500

    axios-setup (#3)

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+describe('First Api Tests', () => {
+    it('Consume GET Service', async () => {
+        const response = await axios.get('https://httpbin.org/ip');
+      
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data).to.have.property('origin');
+    }); 
+
+    it('Consume GET Service with query parameters', async () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+        };
+        
+        const response = await axios.get('https://httpbin.org/get', { query });
+        
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.config.query).to.eql(query);
+    });
+
+    it('Consume POST Service with body', async () => {
+        const body = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+        };
+        
+        const response = await axios.post('https://httpbin.org/post', body);
+        
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(JSON.parse(response.data.data)).to.eql(body);
+    });
+
+    it('Consume DELETE Service with query params', async () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+        };
+        
+        const response = await axios.delete('https://httpbin.org/delete', {query});
+        
+        expect(response.status).to.equal(StatusCodes.OK);
+    });
+});
